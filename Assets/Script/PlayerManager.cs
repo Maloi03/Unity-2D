202502f1e98c1f6d0bd526b1479614ec8b559046
Maloi03 <loiphat2406@gmail.com>
2023-12-13 17:32:44 +0700

    13/12/2023

--- Assets/Script/PlayerManager.cs
+++ Assets/Script/PlayerManager.cs
@@ -4,6 +4,7 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using Cinemachine;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -12,11 +13,17 @@ public class PlayerManager : MonoBehaviour
     public static Vector2 lastCheckPointPos = new Vector2(-8,0);
     public static int numberOfCoins;
     public TextMeshProUGUI coinsText;
+    public CinemachineVirtualCamera virtualCamera;
+    public GameObject[] playerPrefabs;
+    int characterIndex;
     private void Awake()
     {
-        //numberOfCoins = PlayerPrefs.GetInt("NumberOfCoins", 0);
+        characterIndex = PlayerPrefs.GetInt("SelectedCharacter", 0);
+        GameObject player =  Instantiate(playerPrefabs[characterIndex], lastCheckPointPos, Quaternion.identity);
+        virtualCamera.m_Follow = player.transform;
+        numberOfCoins = PlayerPrefs.GetInt("NumberOfCoins", 0); // luu gia tri coins va khong reset coins khi choi van moi(co the de comment)
         isGameOver = false;
-        GameObject.FindGameObjectWithTag("Player").transform.position = lastCheckPointPos;
+        //GameObject.FindGameObjectWithTag("Player").transform.position = lastCheckPointPos;
     }
 
     // Update is called once per frame
